087aec311e996dc351474caa9e0978fb7ec1e9d6
yom-ko <artem.getcode@gmail.com>
2018-09-23 15:46:59 +0300

    Refactor DatePicker into a functional comp

--- src/components/DatePicker.js
+++ src/components/DatePicker.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   Col,
   Label,
@@ -11,130 +11,50 @@ import {
 import DateRangePicker from 'react-bootstrap-daterangepicker';
 import Octicon, { Calendar } from '@githubprimer/octicons-react';
 
-export class DatePicker extends Component {
-  constructor(props) {
-    super(props);
-    this.handleApply = this.handleApply.bind(this);
-    this.state = {
-      startDate: '',
-      endDate: '',
-      status: '',
-      tooltipOpen: false
-    };
-  }
-
-  handleApply(ev, picker) {
-    const { startDate, endDate } = picker;
-    const allowedDurations = [2, 5, 21];
-
-    function IsLeapYear(year) {
-      return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
-    }
-
-    const months = {
-      1: { name: 'Jan', dayCount: 31 },
-      2: { name: 'Feb', dayCount: IsLeapYear(Number(startDate.format('YYYY'))) ? 29 : 28 },
-      3: { name: 'Mar', dayCount: 31 },
-      4: { name: 'Apr', dayCount: 30 },
-      5: { name: 'May', dayCount: 31 },
-      6: { name: 'Jun', dayCount: 30 },
-      7: { name: 'Jul', dayCount: 31 },
-      8: { name: 'Aug', dayCount: 31 },
-      9: { name: 'Sep', dayCount: 30 },
-      10: { name: 'Oct', dayCount: 31 },
-      11: { name: 'Nov', dayCount: 30 },
-      12: { name: 'Dec', dayCount: 31 }
-    };
-
-    const startMonthDayCount = months[Number(startDate.format('MM'))].dayCount;
-
-    if (Number(endDate.format('MM')) === Number(startDate.format('MM'))) {
-      if (
-        !allowedDurations.includes(
-          Number(endDate.format('DD')) - Number(startDate.format('DD')) + 1
-        )
-      ) {
-        this.setState({
-          status: 'is-invalid',
-          tooltipOpen: true
-        });
-      } else {
-        this.setState({
-          status: '',
-          tooltipOpen: false
-        });
-      }
-    } else if (
-      !allowedDurations.includes(
-        Number(endDate.format('DD')) - Number(startDate.format('DD')) + startMonthDayCount + 1
-      )
-    ) {
-      this.setState({
-        status: 'is-invalid',
-        tooltipOpen: true
-      });
-    } else {
-      this.setState({
-        status: '',
-        tooltipOpen: false
-      });
-    }
-
-    this.setState({
-      startDate: startDate.format('YYYY-MM-DD'),
-      endDate: endDate.format('YYYY-MM-DD')
-    });
-  }
-
-  render() {
-    const { startDate, endDate, status, tooltipOpen } = this.state;
-
-    return (
-      <>
-        <Label for="from_until" sm={2}>
-          From / Until
-        </Label>
-        <Col sm={10}>
-          <DateRangePicker
-            containerStyles={{
-              width: '100%'
-            }}
-            onApply={this.handleApply}
+const DatePicker = ({ startDate, endDate, status, tooltipOpen, handleApply }) => (
+  <>
+    <Label for="from_until" sm={2}>
+      From / Until
+    </Label>
+    <Col sm={5}>
+      <DateRangePicker
+        containerStyles={{
+          width: '100%'
+        }}
+        onApply={handleApply}
+      >
+        <InputGroup>
+          <Input
+            type="text"
+            className={status}
+            value={endDate && `${startDate} - ${endDate}`}
+            onChange={() => {}}
+            id="datepicker_input"
+          />
+          <InputGroupAddon addonType="prepend">
+            <InputGroupText
+              style={{
+                borderLeft: 'none',
+                borderTopRightRadius: '5px',
+                borderBottomRightRadius: '5px'
+              }}
+            >
+              <Octicon icon={Calendar} size="small" />
+            </InputGroupText>
+          </InputGroupAddon>
+          <Tooltip
+            target="datepicker_input"
+            placement="bottom"
+            hideArrow
+            style={{ color: '#000', backgroundColor: '#e9ecef' }}
+            isOpen={tooltipOpen}
           >
-            <InputGroup row>
-              <Input
-                type="text"
-                className={status}
-                value={endDate && `${startDate} - ${endDate}`}
-                onChange={() => {}}
-                id="datepicker_input"
-              />
-              <InputGroupAddon addonType="prepend">
-                <InputGroupText
-                  style={{
-                    borderLeft: 'none',
-                    borderTopRightRadius: '5px',
-                    borderBottomRightRadius: '5px'
-                  }}
-                >
-                  <Octicon icon={Calendar} size="small" />
-                </InputGroupText>
-              </InputGroupAddon>
-              <Tooltip
-                target="datepicker_input"
-                placement="bottom"
-                hideArrow
-                style={{ color: '#000', backgroundColor: '#e9ecef' }}
-                isOpen={tooltipOpen}
-              >
-                Select a range of 2, 5, or 21 days (start/end inclusive)!
-              </Tooltip>
-            </InputGroup>
-          </DateRangePicker>
-        </Col>
-      </>
-    );
-  }
-}
+            Select a range of 2, 5, or 21 days (start/end inclusive)!
+          </Tooltip>
+        </InputGroup>
+      </DateRangePicker>
+    </Col>
+  </>
+);
 
 export default DatePicker;
--- src/screens/RequestForm.js
+++ src/screens/RequestForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
@@ -17,48 +17,134 @@ import { history } from 'store';
 import { actions } from 'modules/app';
 import DatePicker from 'components/DatePicker';
 
-const RequestForm = () => (
-  <>
-    <h3 style={{ marginBottom: '2rem' }}>Create request</h3>
-    <Form>
-      <FormGroup row>
-        <Label for="price" sm={2}>
-          Price
-        </Label>
-        <Col sm={10}>
-          <InputGroup>
-            <InputGroupAddon addonType="prepend">$</InputGroupAddon>
-            <Input type="number" step="1" id="price" />
-            <InputGroupAddon addonType="append">.00</InputGroupAddon>
-          </InputGroup>
-        </Col>
-      </FormGroup>
-      <FormGroup row>
-        <Label for="passengers" sm={2}>
-          Passengers
-        </Label>
-        <Col sm={10}>
-          <Input type="number" min="1" step="1" id="passengers" />
-        </Col>
-      </FormGroup>
-      <FormGroup row>
-        <DatePicker />
-      </FormGroup>
-    </Form>
-    <Button
-      style={{
-        backgroundColor: '#58B957',
-        borderColor: '#58B957',
-        padding: '0.450rem 1.8rem',
-        margin: '2rem 0 0 auto',
-        float: 'right'
-      }}
-      onClick={() => history.push('/')}
-    >
-      Create
-    </Button>
-  </>
-);
+export class RequestForm extends Component {
+  constructor(props) {
+    super(props);
+    this.handleApply = this.handleApply.bind(this);
+    this.state = {
+      startDate: '',
+      endDate: '',
+      status: '',
+      tooltipOpen: false
+    };
+  }
+
+  handleApply(ev, picker) {
+    const { startDate, endDate } = picker;
+    const allowedDurations = [2, 5, 21];
+
+    function IsLeapYear(year) {
+      return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+    }
+
+    const months = {
+      1: { name: 'Jan', dayCount: 31 },
+      2: { name: 'Feb', dayCount: IsLeapYear(Number(startDate.format('YYYY'))) ? 29 : 28 },
+      3: { name: 'Mar', dayCount: 31 },
+      4: { name: 'Apr', dayCount: 30 },
+      5: { name: 'May', dayCount: 31 },
+      6: { name: 'Jun', dayCount: 30 },
+      7: { name: 'Jul', dayCount: 31 },
+      8: { name: 'Aug', dayCount: 31 },
+      9: { name: 'Sep', dayCount: 30 },
+      10: { name: 'Oct', dayCount: 31 },
+      11: { name: 'Nov', dayCount: 30 },
+      12: { name: 'Dec', dayCount: 31 }
+    };
+
+    const startMonthDayCount = months[Number(startDate.format('MM'))].dayCount;
+
+    if (Number(endDate.format('MM')) === Number(startDate.format('MM'))) {
+      if (
+        !allowedDurations.includes(
+          Number(endDate.format('DD')) - Number(startDate.format('DD')) + 1
+        )
+      ) {
+        this.setState({
+          status: 'is-invalid',
+          tooltipOpen: true
+        });
+      } else {
+        this.setState({
+          status: '',
+          tooltipOpen: false
+        });
+      }
+    } else if (
+      !allowedDurations.includes(
+        Number(endDate.format('DD')) - Number(startDate.format('DD')) + startMonthDayCount + 1
+      )
+    ) {
+      this.setState({
+        status: 'is-invalid',
+        tooltipOpen: true
+      });
+    } else {
+      this.setState({
+        status: '',
+        tooltipOpen: false
+      });
+    }
+
+    this.setState({
+      startDate: startDate.format('YYYY-MM-DD'),
+      endDate: endDate.format('YYYY-MM-DD')
+    });
+  }
+
+  render() {
+    const { startDate, endDate, status, tooltipOpen } = this.state;
+
+    return (
+      <>
+        <h3 style={{ marginBottom: '2rem' }}>Create request</h3>
+        <Form>
+          <FormGroup row>
+            <Label for="price" sm={2}>
+              Price
+            </Label>
+            <Col sm={5}>
+              <InputGroup>
+                <InputGroupAddon addonType="prepend">$</InputGroupAddon>
+                <Input type="number" step="1" id="price" />
+                <InputGroupAddon addonType="append">.00</InputGroupAddon>
+              </InputGroup>
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <Label for="passengers" sm={2}>
+              Passengers
+            </Label>
+            <Col sm={5}>
+              <Input type="number" min="1" step="1" id="passengers" />
+            </Col>
+          </FormGroup>
+          <FormGroup row>
+            <DatePicker
+              handleApply={this.handleApply}
+              startDate={startDate}
+              endDate={endDate}
+              status={status}
+              tooltipOpen={tooltipOpen}
+            />
+          </FormGroup>
+        </Form>
+        <Button
+          style={{
+            backgroundColor: '#58B957',
+            borderColor: '#58B957',
+            padding: '0.450rem 1.8rem',
+            margin: '2rem 0 0 auto',
+            float: 'right'
+          }}
+          onClick={() => history.push('/')}
+        >
+          Create
+        </Button>
+      </>
+    );
+  }
+}
 
 // Map state and dispatch() to the component props
 const mapStateToProps = () => ({});
